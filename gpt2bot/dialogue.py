b3a1536e157c318da58abd9143bedd0b1b1b85b2
Oleg Polakow <olegpolakow@gmail.com>
2021-01-25 17:24:58 +0100

    Fix response order

--- gpt2bot/dialogue.py
+++ gpt2bot/dialogue.py
@@ -54,7 +54,7 @@ def run(**kwargs):
                     turn['bot1_messages'].append(first_message)
                     print(f"Bot 1:", first_message)
                 for j in range(1, 3):
-                    if i == 0:
+                    if i == 0 and j == 1:
                         continue
                     # Merge turns into a single prompt (don't forget delimiter)
                     prompt = ""
